b4a6107b5b759dae2add7f74489fd048562dea97
Michael Perna <michael.anthony.perna@gmail.com>
2023-03-22 06:24:12 -0700

    fix example and add .vscode to .gitignore

--- example.py
+++ example.py
@@ -1,8 +1,8 @@
 from pydfs_lineup_optimizer import Site, Sport, get_optimizer
 
 
-optimizer = get_optimizer(Site.YAHOO, Sport.BASKETBALL)
-optimizer.load_players_from_csv("yahoo-NBA.csv")
+optimizer = get_optimizer(Site.DRAFTKINGS, Sport.BASEBALL)
+optimizer.load_players_from_csv("DKSalaries.csv")
 lineup_generator = optimizer.optimize(10)
 for lineup in lineup_generator:
     print(lineup)
